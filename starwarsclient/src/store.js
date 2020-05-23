607b6a4ea9f5aa62d8c22e5caf0fd5e8dfc6acaa
nirupamaa jaya <nirustanite@gmail.com>
2020-05-23 20:56:21 +0200

    setup store

---
+++ starwarsclient/src/store.js
@@ -0,0 +1,19 @@
+import 'regenerator-runtime/runtime';
+import { createStore, applyMiddleware, compose } from 'redux'
+import reducer from './store/reducers';
+import createSagaMiddleware from 'redux-saga';
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const sagaMiddleware = createSagaMiddleware()
+
+const enhancer = composeEnhancers(
+  applyMiddleware(sagaMiddleware)
+)
+
+
+const store = createStore(reducer, enhancer)
+
+
+export default store;
